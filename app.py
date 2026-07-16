3009978b2a84fa27a17addd7f1c0178b4ec2a101
Baleen Mohammad <DEINE_NOREPLY_EMAIL>
2026-07-16 22:29:30 +0200

    Fix indentation in app.py

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route("/health")
 def health():
     return jsonify({
         "status": "healthy"
     })
 
+
 if __name__ == "__main__":
-    port =    int(os.getenv("PORT", 5000))
-    app.run    (host="0.0.0.0", port=port)  # nosec
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

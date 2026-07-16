6988a26ea6b6b58f3a7873858ebd0d08d9e373d0
Baleen Mohammad <DEINE_NOREPLY_EMAIL>
2026-07-16 14:35:17 +0200

    security: ignore false positive B104 with nosec

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 import os
@@ -19,4 +18,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)  # nosec

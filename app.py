7ddd521779579224596749d90529bb4011114103
Baleen Mohammad <DEINE_NOREPLY_EMAIL>
2026-07-16 15:06:54 +0200

    fix: correct python indentation in app.py

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-   app.run(host="0.0.0.0", port=port)  # nosec
+    app.run(host="0.0.0.0", port=port)  # nosec

4e6ad2f267fb16037e2c81abe595910a34884a45
Baleen Mohammad <DEINE_NOREPLY_EMAIL>
2026-07-16 15:20:07 +0200

    fix: correct indentation in app.py

--- app.py
+++ app.py
@@ -16,10 +16,7 @@ def health():
         "status": "healthy"
     })
 
-if __name__ == "__main__":
-port =     int(os.getenv("PORT", 5000))
-app.run    (host="0.0.0.0", port=port)  # nosec
-
+i
 if __name__ == "__main__":
     port =    int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)  # nosec
